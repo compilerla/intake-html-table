d3e9c14030415cbfeccd319fc55f3e0ebfc6354f
Kegan Maher <kegan@compiler.la>
2021-07-14 00:01:36 +0000

    test: read in a variety of scenarios

--- tests/test_html_table.py
+++ tests/test_html_table.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 from intake_html_table import HtmlTableSource
@@ -50,3 +51,62 @@ def test_open(table_example):
     for attr, value in expected.items():
         assert hasattr(src, attr)
         assert getattr(src, attr) == value
+
+
+def test_read_single(table_example):
+    expected_df = pd.read_html(examples.table_path())[0]
+
+    src = table_example()
+
+    src.discover()
+    assert src.npartitions == 1
+
+    df = src.read()
+    assert len(src.dataframes) == 1
+    assert expected_df.equals(df)
+
+
+def test_read_multi(document_example):
+    expected_df = pd.concat(pd.read_html(examples.document_path()))
+
+    src = document_example()
+
+    src.discover()
+    assert src.npartitions == 2
+
+    df = src.read()
+    assert len(src.dataframes) == 2
+    assert expected_df.equals(df)
+
+
+def test_read_kwargs(document_example):
+    attrs = {"id": "data"}
+    skiprows = 2
+    expected_df = pd.read_html(examples.document_path(), attrs=attrs, skiprows=skiprows)[0]
+
+    src = document_example(attrs=attrs, skiprows=skiprows)
+
+    src.discover()
+    assert src.npartitions == 1
+
+    df = src.read()
+    assert len(src.dataframes) == 1
+    assert expected_df.equals(df)
+
+
+def test_read_partition(document_example):
+    example_dfs = pd.read_html(examples.document_path())
+    expected_df1 = example_dfs[0]
+    expected_df2 = example_dfs[1]
+
+    src = document_example()
+
+    src.discover()
+    assert src.npartitions == 2
+
+    # Read partitions is opposite order
+    df2 = src.read_partition(1)
+    df1 = src.read_partition(0)
+
+    assert expected_df1.equals(df1)
+    assert expected_df2.equals(df2)
