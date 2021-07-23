db32c2b104c1c7b6cf80eeb8013f54d9594342f6
Kegan Maher <kegan@compiler.la>
2021-07-23 00:07:33 +0000

    refactor(tests): simplify helper
    no reason to look around, we know where the examples directory is

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,19 +6,8 @@ import pytest
 def get_path(path):
     """Get the `path` under the `examples/` directory, with respect to this file."""
     root = Path(__file__).parent
-    examples = "./examples"
-    if root.joinpath(examples).exists():
-        examples = root.joinpath(examples)
-    else:
-        while root.joinpath("..").exists():
-            root = root.joinpath("..")
-            if root.joinpath(examples).exists():
-                examples = root.joinpath(examples)
-                break
-        else:
-            raise FileNotFoundError("examples/ directory cannot be found")
-
-    return examples.joinpath(path).resolve()
+    examples = "../examples"
+    return root.joinpath(examples).joinpath(path).resolve()
 
 
 @pytest.fixture
